775621adb114119da351cf6eb38f8c0242e5df3e
SaulFromRobotics <SaulRS951@protonmail.com>
2024-07-01 18:56:44 -0700

    write in my understanding of the google sheets api

--- script.py
+++ script.py
@@ -1,14 +1,28 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i python3 -p python3Packages.slack-bolt
-from datetime import datetime, date, time
+#! nix-shell -i python3 -p python312Packages.slack-bolt python312Packages.google-api-python-client python312Packages.google-auth-httplib2 python312Packages.google-auth-oauthlib
+from os import path
 from re import search
+from datetime import datetime, date, time
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
-# read and open secrets, parsing them as if they where in the dotenv format
+# read and open basic secrets, parsing them as if they where in the dotenv format
 secretsFile = open("./secrets", "r") # requires BOT_TOKEN, SIGNING_SECRET, and APP_TOKEN
-secrets = { x.split("=")[0]: x.split("=")[1][:-1] for x in secretsFile.readlines() }
+secrets = { x.split("=")[0]: x.split("=")[1].replace("\n","") for x in secretsFile.readlines() }
 secretsFile.close() # forgetting to do this leaks something
+
+# process google's strange secret system and create a lambda to simplify the madness
+SCOPES = [ "https://www.googleapis.com/auth/spreadsheets" ] # If modifying these scopes, delete the file sheets.json
+if path.exists("sheets.json"): creds = Credentials.from_authorized_user_file("sheets.json", SCOPES)
+if 'creds' not in locals() or not creds.valid: # If there are no (valid) credentials available, let the user log in
+	if 'creds' in locals() and creds.expired and creds.refresh_token: creds.refresh(Request())
+	else: creds = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES).run_local_server(port=0)
+	with open("sheets.json", "w") as token: token.write(creds.to_json())
+sheet=lambda method,args:getattr(build("sheets","v4",credentials=creds).spreadsheets().values(),method)(**({"spreadsheetId":secrets["SHEET_ID"]}|args)).execute()
 
 app = App(token=secrets["BOT_TOKEN"], signing_secret=secrets["SIGNING_SECRET"])
 
@@ -16,7 +30,7 @@ app = App(token=secrets["BOT_TOKEN"], signing_secret=secrets["SIGNING_SECRET"])
 def handleNewCommand(ack, command, say): # bolt commands need to be passed as arguments
 	ack() # the api is a needy freak and demands constant acknowledgment
 	try: # grab the time data in a way that enforces the correct format is being used
-		times = search("^([0-9]+)\:([0-9]+)(am|pm) - ([0-9]+)\:([0-9]+)(am|pm)$", command["text"]).groups()
+		times = search("^([0-9]+):([0-9]+)(am|pm) - ([0-9]+):([0-9]+)(am|pm)$", command["text"]).groups()
 	except AttributeError: # this fails if the user does not follow the correct format
 		say("You formatted the message incorrectly. Please reference the example.")
 		return # complain to user and exit
@@ -32,4 +46,5 @@ def handleNewCommand(ack, command, say): # bolt commands need to be passed as ar
 	hours = (end-begin).seconds / (60**2) # get the number of hours the user attended the meeting
 	say(f"{command['user_name']} {today} {hours}") # react to user by saying the info they just gave us
 
+print(sheet("get", {"range":"Fall Attendance!A1:Z3"})["values"])
 SocketModeHandler(app, secrets["APP_TOKEN"]).start() # socket mode is superior in every way dont worry about it
