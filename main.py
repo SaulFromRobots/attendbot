ad848986941b40384c65db4ac96e6cc9f550007d
SaulFromRobotics <SaulRS951@protonmail.com>
2024-07-02 20:42:10 -0700

    add a seperate /meeting command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i python3 -p python312Packages.slack-bolt python312Packages.google-api-python-client python312Packages.google-auth-httplib2 python312Packages.google-auth-oauthlib
+from datetime import date
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from googleapiclient.discovery import build
@@ -13,7 +14,7 @@ sheetsAPI=lambda method,args:getattr(build("sheets","v4",credentials=sheetsCreds
 app = App(token=secrets["BOT_TOKEN"], signing_secret=secrets["SIGNING_SECRET"])
 
 @app.command("/attend") # `/attend` is the main command. This is the one thing the bot does, so it's fine.
-def handleNewCommand(ack, command, say): # bolt commands need to be passed as arguments
+def attend(ack, command, say): # bolt commands need to be passed as arguments
 	ack() # the api is a needy freak and demands constant acknowledgment
 
 	try: day, hours = getInfo.dayHours(command["text"]) # use my function to extract the day and hours attended
@@ -24,9 +25,19 @@ def handleNewCommand(ack, command, say): # bolt commands need to be passed as ar
 	except: return say("Something went wrong finding your name in the spreadsheet.")
 
 	row, needWriteDate = getInfo.findDayRow(sheetsAPI("get", {"range":"Fall Attendance!A4:A","majorDimension":"COLUMNS"}), day)
-	if (needWriteDate): sheetsAPI("update",{"range":f"Fall Attendance!A{row}:A{row}","valueInputOption":"RAW","body":{"values":[[day]]}}) # Write the date in the row if it wasn't found
+	if (needWriteDate): return say("{day} is not on the spreadsheet, attendance is not being counted for that day.")
 
 	sheetsAPI("update",{"range":f"Fall Attendance!{col}{row}:{col}{row}","valueInputOption":"RAW","body":{"values":[[hours]]}})
-	say(f"sheet[{col}{row}] = {hours} (needWriteDate = {needWriteDate})") # react to user by saying the info they just gave us
+	say(f"sheet[{col}{row}] = {hours}") # react to user by saying the info they just gave us
+
+@app.command("/meeting")
+def attend(ack, command, say):
+	ack()
+	day = date.today().strftime("%-m/%-d/%Y") if command["text"] == "" else command["text"]
+	row, needWriteDate = getInfo.findDayRow(sheetsAPI("get", {"range":"Fall Attendance!A4:A","majorDimension":"COLUMNS"}), day)
+	if (needWriteDate):
+		sheetsAPI("update",{"range":f"Fall Attendance!A{row}:A{row}","valueInputOption":"RAW","body":{"values":[[day]]}})
+		say(f"{day} added to the spreadsheet.")
+	else: say(f"{day} is already row {row} in the spreadsheet.")
 
 if __name__ == "__main__": SocketModeHandler(app, secrets["APP_TOKEN"]).start() # socket mode is superior in every way dw abt it
