336da6e30bc55cbeb4174fcbda6f4076e9f37d32
SaulFromRobots <saulrs951@protonmail.com>
2024-12-03 16:58:29 -0800

    reimplement attendance reporting in the modal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date, time
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from googleapiclient.discovery import build
@@ -8,6 +8,8 @@ import getInfo
 # a few helper objects
 sheetsAPI=lambda method,args:getattr(build("sheets","v4",credentials=googleAuth).spreadsheets().values(),method)(**({"spreadsheetId":k["SHEET"]}|args)).execute() # create a lambda to simplify the madness
 k = keys.copy() # use a copy of keys instead of the raw object
+
+
 
 # declare a slack app
 app = App(token=k["BOT_TOKEN"], signing_secret=k["SIGNING_SECRET"])
@@ -20,6 +22,7 @@ def attend_modal(ack, shortcut, client):
 		trigger_id=shortcut["trigger_id"],
 		view = 	{
 			"type": "modal",
+			"callback_id": "attendance-taker",
 			"title": { "type": "plain_text", "text": "Attend", "emoji": True },
 			"submit": { "type": "plain_text", "text": "Submit", "emoji": True },
 			"close": { "type": "plain_text", "text": "Cancel", "emoji": True },
@@ -40,7 +43,7 @@ def attend_modal(ack, shortcut, client):
 						"type": "timepicker",
 						"initial_time": "12:00",
 						"placeholder": { "type": "plain_text", "text": "Select time", "emoji": True },
-						"action_id": "timepicker-action"
+						"action_id": "timepicker-arrive-action"
 					},
 					"label": { "type": "plain_text", "text": "Arrive time", "emoji": True }
 				},
@@ -50,7 +53,7 @@ def attend_modal(ack, shortcut, client):
 						"type": "timepicker",
 						"initial_time": datetime.today().strftime("%H:%M"),
 						"placeholder": { "type": "plain_text", "text": "Select time", "emoji": True },
-						"action_id": "timepicker-action"
+						"action_id": "timepicker-leave-action"
 					},
 					"label": { "type": "plain_text", "text": "Leave time", "emoji": True }
 				}
@@ -58,10 +61,31 @@ def attend_modal(ack, shortcut, client):
 		}
 	)
 
-@app.view("")
-def handle_view_submission_events(ack, body, view):
-    ack()
-    print(body["user"]["name"], view["state"]["values"])
+@app.view("attendance-taker")
+def handle_view_submission_events(ack, body, view, client):
+	ack()
+	user = body["user"]["name"]
+	response = dict(map(dict.popitem,view["state"]["values"].values()))
+	day = "{1}/{2}/{0}".format(*(response["datepicker-action"]["selected_date"].split("-")))
+	start = datetime.strptime(response["timepicker-arrive-action"]["selected_time"], r"%H:%M")
+	end = datetime.strptime(response["timepicker-leave-action"]["selected_time"], r"%H:%M")
+	hours = ((end-start).seconds / (60**2)) - (start < datetime.combine(date.today(), time(hour=12)) < end)
+	
+	print(user, day, hours, response)
+	message = lambda msg: client.views_update(
+		view_id=body["view"]["id"],
+		hash=body["view"]["hash"],
+		view={"type": "modal","callback_id": "complain","title": {"type": "plain_text", "text": "Attendbot"},"blocks": [{"type": "section","text": {"type": "plain_text", "text": msg}}]}
+	) # TODO: this is broken
+
+	try: col = getInfo.letter(sheetsAPI("get",{"range":f"{k['TABLE']}!C1:1"})["values"][0].index(user)+3)
+	except: return message("Something went wrong finding your name in the spreadsheet.")
+
+	row, needWriteDate = getInfo.findDayRow(sheetsAPI("get", {"range":f"{k['TABLE']}!A4:A","majorDimension":"COLUMNS"}), day)
+	if (needWriteDate): return message(f"{day} is not on the spreadsheet, attendance is not being counted for that day.")
+
+	sheetsAPI("update",{"range":f"{k['TABLE']}!{col}{row}:{col}{row}","valueInputOption":"RAW","body":{"values":[[hours]]}})
+	return message(f"You attended on {day} for {hours} hours.")
 
 @app.command("/attend") # `/attend` is the main command. This is the one thing the bot does, so it's fine.
 def attend(ack, command, say): # bolt commands need to be passed as arguments
