b6d698dc51df1e164f26f0acd0ff791098a3e68d
SaulFromRobots <saulrs951@protonmail.com>
2024-12-31 23:41:52 -0800

    setting injection

--- flake.nix
+++ flake.nix
@@ -4,14 +4,11 @@
 
 	outputs = { self, nixpkgs }: let
 		pkgs = nixpkgs.legacyPackages.x86_64-linux;
-		deps = pkgs.python312.withPackages (p: with p; [
-			slack-bolt google-api-python-client google-auth-httplib2 google-auth-oauthlib
-		]);
-		in {
+	in {
 		devShells.x86_64-linux.default = pkgs.mkShell {
-			nativeBuildInputs = [ deps ];
+			nativeBuildInputs = [ (pkgs.python312.withPackages (p: with p; [ slack-bolt google-api-python-client google-auth-httplib2 google-auth-oauthlib ])) ];
 		};
-		packages.x86_64-linux = {
+		packages.x86_64-linux = rec {
 			default = with pkgs.python312Packages; buildPythonPackage {
 				pname = "attendbot";
 				version = "main";
@@ -23,8 +20,7 @@
 			image = pkgs.dockerTools.buildImage {
 				name = "attendbot";
 				tag = "latest";
-				copyToRoot = [ ./. ];
-				config.Cmd = [ (self.default+"/bin/attendbot") ];
+				config.Cmd = [ (default+"/bin/attendbot") ];
 			};
 		};
 	};
--- src/settings.py
+++ src/settings.py
@@ -1,19 +1,30 @@
-from os import path
+from os import path, makedirs, environ
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from json import loads
 
-optsFile = open("./options.json", "r") # requires BOT_TOKEN, SIGNING_SECRET, and APP_TOKEN
+if not path.exists("conf"):
+	makedirs("conf")
+	for confFile in [ "CREDENTIALS", "OPTIONS", "SHEETS" ]:
+		jsonFile = open(f"conf/{confFile.lower()}.json", "x")
+		jsonFile.write(environ.get(confFile))
+		jsonFile.close()
+
+optsFile = open("./conf/options.json", "r") # requires BOT_TOKEN, SIGNING_SECRET, and APP_TOKEN
 opts = loads(optsFile.read())
 optsFile.close() # forgetting to do this leaks something
 
 # process google's strange secret system
 SCOPES = [ "https://www.googleapis.com/auth/spreadsheets" ] # If modifying these scopes, delete the file sheets.json
-if path.exists("sheets.json"): googleAuth = Credentials.from_authorized_user_file("sheets.json", SCOPES)
+if path.exists("conf/sheets.json"):
+	googleAuth = Credentials.from_authorized_user_file("conf/sheets.json", SCOPES)
 if 'googleAuth' not in locals() or not googleAuth.valid: # If there are no (valid) credentials available, let the user log in
-	if 'googleAuth' in locals() and googleAuth.expired and googleAuth.refresh_token: googleAuth.refresh(Request())
-	else: googleAuth = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES).run_local_server(port=0)
-	with open("sheets.json", "w") as token: token.write(googleAuth.to_json())
+	if 'googleAuth' in locals() and googleAuth.expired and googleAuth.refresh_token:
+		googleAuth.refresh(Request())
+	else:
+		googleAuth = InstalledAppFlow.from_client_secrets_file("conf/credentials.json", SCOPES).run_local_server(port=0)
+	with open("conf/sheets.json", "w") as token:
+		token.write(googleAuth.to_json())
 sheetsCreds = build("sheets","v4",credentials=googleAuth).spreadsheets()
